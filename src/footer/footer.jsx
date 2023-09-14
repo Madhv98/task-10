bb4921d8f2a9cbf41974fd355be2362fbdc822f3
Madhav Dhital <mdhital@madhavs-mbp.mynetworksettings.com>
2023-09-14 01:20:05 -0400

    Name list and box

---
+++ src/footer/footer.jsx
@@ -0,0 +1,12 @@
+import styles from './footer.module.css'
+
+function Footer() {
+    return (
+      <div className={styles.footer}>
+        <p>Lists</p>
+        </div>
+    )
+}
+
+    
+    export default Footer
